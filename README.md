a137a42abdf5aa3c26a3f681d823a7c145a46a1c
DanielSimonsen90 <danielsimonsen90@gmail.com>
2023-09-18 21:16:58 +0200

    Finsih v. 1.0.0

---
+++ README.md
@@ -0,0 +1,16 @@
+# BEGONE SPINS
+*By [Danhosaur](<https://namemc.com/profile/Danhosaur.2>) / [DanielSimonsen90](<https://github.com/DanielSimonsen90>)*
+
+Minecraft mod specifically made for people, who suffer from arachnophobia. Completely removes the entity and all its relations from the game.
+
+## Installation
+You can download the mod for 1.20.1 [here](./versions/1.20.1/begone_spins-1.0.0-1.20.1.jar). Can't promise it'll work on earlier or later Minecraft versions, but regardless feel free to [open an issue](<https://github.com/DanielSimonsen90/begone_spins/issues>).
+
+For installing the mod into your minecraft game, make sure you have already played Minecraft 1.20.1 and have installed [Forge](<https://files.minecraftforge.net/net/minecraftforge/forge/index_1.20.1.html>).
+Once Forge has been installed, you can now search `%appdata%` on your computer, and navigate to the `.minecraft` folder, ensure you have a `mods` folder, and put the mod in there.
+
+## Changes
+* Prevents entity types from spawning completely. Spawn eggs, spawners & `/summon` will not work.
+* Prevents entity sounds from being played using `/playsound`
+* Changed item names that include the name of the entity to either "Spin" or removed the entity name completely.
+* Zombies can now drop string & eyes.
--- src/main/java/com/danho/begone_spins/BegoneSpins.java
+++ src/main/java/com/danho/begone_spins/BegoneSpins.java
@@ -1,27 +1,19 @@
 package com.danho.begone_spins;
 
-import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.FormattedText;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.*;
 import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraftforge.client.event.sound.PlaySoundEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.EntityEvent;
 import net.minecraftforge.event.entity.living.LivingDropsEvent;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 import java.util.List;
-
-import static net.minecraft.network.chat.Component.*;
-//import org.slf4j.Logger;
-//import com.mojang.logging.LogUtils;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(BegoneSpins.MOD_ID)
@@ -29,43 +21,55 @@ public class BegoneSpins
 {
     // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "begone_spins";
-    // Directly reference a slf4j logger
-    // private static final Logger LOGGER = LogUtils.getLogger();
-    // Create a Deferred Register to hold Blocks which will all be registered under the "begone_spins" namespace
     public BegoneSpins()
     {
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-
-        // Register the commonSetup method for modloading
-        modEventBus.addListener(this::commonSetup);
-
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
     }
 
-    private void commonSetup(final FMLCommonSetupEvent event)
+    /**
+     * Prevents spider sounds from playing
+     * @param event Sound event
+     */
+    @SubscribeEvent
+    public void onSoundPlayed(final PlaySoundEvent event)
     {
-
+        // If the sound name contains "spider", cancel the event
+        if (event.getName().contains("spider")) {
+            event.setCanceled(true);
+        }
     }
 
+    /**
+     * Prevents spiders from spawning
+     * @param event Entity event
+     */
     @SubscribeEvent
     public void onSpiderSpawn(final EntityEvent event)
     {
+        // If no entity is being spawned, return
         Entity entity = event.getEntity();
         if (entity == null) return;
 
+        // If the entity is a type of spider, cancel the event
         EntityType entityType = entity.getType();
         List<EntityType> spiderTypes = List.of(EntityType.SPIDER, EntityType.CAVE_SPIDER);
         if (spiderTypes.contains(entityType))
             event.setCanceled(true);
     }
 
+    /**
+     * Makes zombies drop string and spider eyes
+     * @param event Living drops event
+     */
     @SubscribeEvent
     public void onZombieTypeKilled(final LivingDropsEvent event)
     {
+        // If no entity is being killed, return
         Entity entity = event.getEntity();
         if (entity == null) return;
 
+        // If the entity is not a type of zombie, return
         EntityType entityType = entity.getType();
         List<EntityType> zombieTypes = List.of(
                 EntityType.ZOMBIE,
@@ -75,32 +79,52 @@ public class BegoneSpins
                 EntityType.ZOMBIFIED_PIGLIN);
         if (!zombieTypes.contains(entityType)) return;
 
+        // If the entity was not killed by a player, return
         Entity entityCausedDeath = event.getSource().getEntity();
         if (!(entityCausedDeath instanceof Player player)) return;
 
+        // Determine if mob should drop string and/or spider eye depending on random chance and player's hand item
         ItemStack mainHandItem = player.getMainHandItem();
-        boolean shouldDropString = shouldDropItem(mainHandItem, 0.5); // 50% chance
-        boolean shouldDropEye = shouldDropItem(mainHandItem, 0.1); // 10% chance
+        boolean shouldDropString = shouldDropItem(mainHandItem, 0.5); // 50% base chance
+        boolean shouldDropEye = shouldDropItem(mainHandItem, 0.1); // 10% base chance
 
+        // Add item entities to drops, if applicable
         if (shouldDropString) event.getDrops().add(createItemEntity(entity, Items.STRING, mainHandItem));
         if (shouldDropEye) event.getDrops().add(createItemEntity(entity, Items.SPIDER_EYE, mainHandItem));
     }
 
+    /**
+     * Determines if a mob should drop an item based on the player's hand item and a random chance
+     * @param handItem The player's hand item
+     * @param chance The base chance of the item dropping
+     * @return Whether the item should drop
+     */
     private boolean shouldDropItem(ItemStack handItem, double chance) {
+        // Increase chance if player is holding a sword or axe
         Item item = handItem.getItem();
         if (item instanceof SwordItem || item instanceof AxeItem) {
             chance += 0.05;
         }
 
+        // Increase chance if player has looting enchantment
         int lootingLevel = handItem.getEnchantmentLevel(Enchantments.MOB_LOOTING);
         if (lootingLevel > 0) {
             chance += 0.1 * lootingLevel;
         }
 
+        // Return whether the random chance was met
         return Math.random() < chance;
     }
 
+    /**
+     * Create an ItemEntity to add to the drops
+     * @param entity The entity to create the item entity at
+     * @param item The item to create the item entity with
+     * @param handItem The player's hand item
+     * @return The created item entity
+     */
     private ItemEntity createItemEntity(Entity entity, Item item, ItemStack handItem) {
+        // Modify item drop count based on looting enchantment
         int lootingLevel = handItem.getEnchantmentLevel(Enchantments.MOB_LOOTING);
         int count = 1 + (int) (Math.random() * lootingLevel);
         return new ItemEntity(entity.level(), entity.getX(), entity.getY(), entity.getZ(), new ItemStack(item, count));
